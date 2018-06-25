6009643ea659652e9a53ee9846999f602b8e66b6
Neil Grogan <neil@grogan.ie>
2018-06-25 17:44:34 +0100

    Add youtube download script

---
+++ config/webhook/yt-dl.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+test -z $1 && echo "need video link! $0 <video link>" && exit 1
+test -z $2 && echo "need directory destination! $0 <video link> <destination>" && exit 1
+
+if [[ $2 = *"Music"* ]]; then
+    echo "It's music!"
+    TITLE=$(youtube-dl -o '/downloads/complete/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title)
+    $BEET import /downloads/complete/Music
+    plex-refresh 2
+elif [[ $2 = *"Video"* ]]; then
+    TITLE=$(youtube-dl -o '/media/Videos/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title)
+    plex-refresh 7
+fi
+
+/usr/bin/pushover "$TITLE is complete"
+
