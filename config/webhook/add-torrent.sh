f45fcb6964a252f38dda9bce8da22fd062a74d1f
Neil Grogan <neil@grogan.ie>
2018-06-25 17:07:32 +0100

    Update add-torrent.sh

--- config/webhook/add-torrent.sh
+++ config/webhook/add-torrent.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 test -z $1 && echo "need magnet link! $0 <magnet link>" && exit 1
- 
+test -z $2 && echo "need directory destination! $0 <magnet link> <destination>" && exit 1
+
+$DOWNLOAD_DIR=/download/complete
+
 # HOST=trans
 # PORT=9091
 TRANSMISSION=/usr/bin/transmission-cli
@@ -8,4 +11,4 @@ TRANSMISSION=/usr/bin/transmission-cli
 # SESSID=$(curl --silent --anyauth  "http://$HOST:$PORT/transmission/rpc" | sed 's/.*<code>//g;s/<\/code>.*//g')
 # curl --silent --anyauth --header "$SESSID" "http://$HOST:$PORT/transmission/rpc" -d "{\"method\":\"torrent-add\",\"arguments\":{\"paused\":\"false\",\"filename\":\"$1\"}}"
 
-$TRANSMISSION $1
+"$TRANSMISSION" "$1" -w "$DOWNLOAD_DIR"/"$2"
