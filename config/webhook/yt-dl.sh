15f0c69aa82e82fead871791008da5b7271c91ec
Neil Grogan <neil@grogan.ie>
2018-07-08 20:11:30 +0100

    Updates to yt-dl script to fix paths

--- config/webhook/yt-dl.sh
+++ config/webhook/yt-dl.sh
@@ -4,10 +4,11 @@ exec &>/dev/stdout
 
 test -z $1 && echo "need video link! $0 <video link>" && exit 1
 test -z $2 && echo "need directory destination! $0 <video link> <destination>" && exit 1
+BEET=/usr/local/bin/beet
 
 if [[ $2 = *"Music"* ]]; then
     echo "It's music!"
-    TITLE=$(youtube-dl -o '/downloads/complete/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title)
+    TITLE=$(youtube-dl --extract-audio --audio-format mp3 -o '/downloads/complete/Music/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title)
     $BEET import /downloads/complete/Music
     plex-refresh 2
 elif [[ $2 = *"Video"* ]]; then
