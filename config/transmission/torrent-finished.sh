ed4bf5bfb54885425dfb44961970dc21150bd649
Neil Grogan <neil@grogan.ie>
2018-10-26 20:05:37 +0100

    Change perms on downloaded

--- config/transmission/torrent-finished.sh
+++ config/transmission/torrent-finished.sh
@@ -6,7 +6,7 @@ TRANS_BIN="/usr/bin/transmission-remote"
 
 "$TRANS_BIN" -t "$TR_TORRENT_ID" --remove
 
-chown 1000:1000 -R "${TR_TORRENT_DIR}"/"${TR_TORRENT_NAME}"
+chown 1000:1000 -R "${TR_TORRENT_DIR}"
 
 if [[ $TR_TORRENT_DIR = *"Music"* ]]; then
     echo "It's music!"
