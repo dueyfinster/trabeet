3ce5fd53f60e1d93594355fbb94c4f6d036e0d59
Neil Grogan <neil@grogan.ie>
2018-10-26 19:21:09 +0100

    Added templated hooks

--- config/transmission/torrent-finished.sh
+++ config/transmission/torrent-finished.sh
@@ -11,10 +11,10 @@ if [[ $TR_TORRENT_DIR = *"Music"* ]]; then
     $BEET import /downloads/complete/Music
     plex-refresh 2
 elif [[ $TR_TORRENT_DIR = *"Movie"* ]]; then
-    filebot -rename --db TheMovieDB --output /media/Movies /downloads/complete/Movies
+    filebot -rename -non-strict --format "{plex}" --log all --db TheMovieDB --output /media/Movies $TR_TORRENT_DIR
     plex-refresh 1
 elif [[ $TR_TORRENT_DIR = *"TV"* ]]; then
-    filebot -rename --db TheTVDB --output /media/TV /downloads/complete/TV
+    filebot -rename -non-strict --format "{plex}" --log all --db TheTVDB --output /media/TV $TR_TORRENT_DIR
     plex-refresh 3
 fi
 
