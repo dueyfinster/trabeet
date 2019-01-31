39697085e63687d15c9370bc71ef43dc931721f2
Neil Grogan <neil@grogan.ie>
2019-01-31 10:18:14 +0000

    Add metadata to audio ripped with yt-dl

--- config/webhook/yt-dl.sh
+++ config/webhook/yt-dl.sh
@@ -8,8 +8,8 @@ BEET=/usr/local/bin/beet
 
 if [[ $2 = *"Music"* ]]; then
     echo "It's music!"
-    TITLE=$(youtube-dl --extract-audio --audio-format mp3 -o '/downloads/complete/Music/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title)
-    $BEET import -q /downloads/complete/Music
+    TITLE=$(youtube-dl --add-metadata -x --extract-audio --audio-format mp3 -o '/downloads/complete/Music/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title)
+    $BEET import -q /downloads/complete/Music/$TITLE
     plex-refresh 2
 elif [[ $2 = *"Video"* ]]; then
     TITLE=$(youtube-dl -o '/media/Videos/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title)
