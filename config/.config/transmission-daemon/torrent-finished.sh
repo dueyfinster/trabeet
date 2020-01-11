59c32e7c4b73195972be6ae226d5cf29fcfbe407
Neil Grogan <neil@grogan.ie>
2020-01-11 20:33:08 +0000

    Clean up variables in torrent-finished.sh

--- config/.config/transmission-daemon/torrent-finished.sh
+++ config/.config/transmission-daemon/torrent-finished.sh
@@ -3,21 +3,24 @@ set -x
 
 BEET=/usr/local/bin/beet
 TRANS_BIN="/usr/bin/transmission-remote"
-
+FILEBOT_CMD="filebot -rename -non-strict --format \"{plex}\" --log all --output /video"
 "$TRANS_BIN" -t "$TR_TORRENT_ID" --remove
 
 chown 1000:1000 -R "${TR_TORRENT_DIR}"
 
-if [[ $TR_TORRENT_DIR = *"Music"* ]]; then
+local TORRENT_DIR=$(echo $TR_TORRENT_DIR | tr '[:upper:]' '[:lower:]')
+local TORRENT_FULL_PATH="${TR_TORRENT_DIR}/${TR_TORRENT_NAME}"
+
+if [[ $TORRENT_DIR = *"music"* ]]; then
     echo "It's music!"
-    $BEET import "${TR_TORRENT_DIR}"/"${TR_TORRENT_NAME}" && \
+    $BEET import "${TORRENT_FULL_PATH}" && \
         plex-refresh $PLEX_MUSIC_LIB_ID
-elif [[ $TR_TORRENT_DIR = *"Movie"* ]]; then
-    filebot -rename -non-strict --format "{plex}" --log all --db TheMovieDB --output /video -r "${TR_TORRENT_DIR}"/"${TR_TORRENT_NAME}" && \
-        plex-refresh $PLEX_MOVIES_LIB_ID
-elif [[ $TR_TORRENT_DIR = *"TV"* ]]; then
-    filebot -rename -non-strict --format "{plex}" --log all --db TheTVDB --output /video -r "${TR_TORRENT_DIR}"/"${TR_TORRENT_NAME}" && \
-        plex-refresh $PLEX_TV_LIB_ID
+elif [[ $TORRENT_DIR = *"movie"* ]]; then
+    echo "It's a movie!"
+    $FILEBOT_CMD --db TheMovieDB -r "${TORRENT_FULL_PATH}" && plex-refresh $PLEX_MOVIES_LIB_ID
+elif [[ $TORRENT_DIR = *"tv"* ]]; then
+    echo "It's a tv show!"
+    $FILEBOT_CMD --db TheTVDB -r "${TORRENT_FULL_PATH}" && plex-refresh $PLEX_TV_LIB_ID
 fi
 
 /usr/bin/pushover "$TR_TORRENT_NAME is complete"
