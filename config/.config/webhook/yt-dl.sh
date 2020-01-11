e677fa1719da05ad3972f71badc32ac98258272a
Neil Grogan <neil@grogan.ie>
2020-01-11 23:38:02 +0000

    Added local vars for kind in yt-dl script

--- config/.config/webhook/yt-dl.sh
+++ config/.config/webhook/yt-dl.sh
@@ -4,27 +4,28 @@ exec &>/dev/stdout
 
 test -z $1 && echo "need video link! $0 <video link>" && exit 1
 test -z $2 && echo "need directory destination! $0 <video link> <destination>" && exit 1
-BEET=/usr/local/bin/beet
 
-if [[ $2 = *"Audio"* ]]; then
+BEET=/usr/local/bin/beet
+FB_CMD="filebot -rename -non-strict --format \"{plex}\" --log all --output /video"
+
+KIND=$(echo $2 | tr '[:upper:]' '[:lower:]')
+
+if [[ $KIND = *"audio"* ]]; then
     TITLE=$(youtube-dl --add-metadata -x --extract-audio --audio-format mp3 -o '/downloads/music/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title) && \
         $BEET import -q /downloads/music && \
         plex-refresh $PLEX_MUSIC_LIB_ID
-elif [[ $2 = *"Movie"* ]]; then
+elif [[ $KIND = *"movie"* ]]; then
     TITLE=$(youtube-dl --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 -o '/downloads/video/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title) && \
-        filebot -rename -non-strict --format "{plex}" --log all --db TheMovieDB --output /video -r /downloads/video/"${TITLE}" && \
-        plex-refresh $PLEX_MOVIES_LIB_ID
-elif [[ $2 = *"TV"* ]]; then
+        $FB_CMD --db TheMovieDB -r /downloads/video/"${TITLE}" && plex-refresh $PLEX_MOVIES_LIB_ID
+elif [[ $KIND = *"tv"* ]]; then
     TITLE=$(youtube-dl --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 -o '/downloads/video/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title) && \
-        filebot -rename -non-strict --format "{plex}" --log all --db TheTVDB --output /video -r /downloads/video/"${TITLE}" && \
-        plex-refresh $PLEX_TV_LIB_ID
-elif [[ $2 = *"Music"* ]]; then
+        $FB_CMD --db TheTVDB -r /downloads/video/"${TITLE}" && plex-refresh $PLEX_TV_LIB_ID
+elif [[ $KIND = *"music"* ]]; then
     TITLE=$(youtube-dl --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 -o '/video/music/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title) && \
         plex-refresh $PLEX_MUSIC_VIDEO_LIB_ID
-elif [[ $2 = *"Talks"* ]]; then
+elif [[ $KIND = *"talks"* ]]; then
     TITLE=$(youtube-dl --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 -o '/video/talks/%(title)s-%(id)s.%(ext)s' --print-json --no-warnings "$1" | jq -r .title) && \
         plex-refresh $PLEX_TALKS_LIB_ID
 fi
 
 /usr/bin/pushover "$TITLE is complete"
-
