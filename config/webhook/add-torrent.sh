bbc0d7ff18fc3ebd53ade86bc2338ffb92c93421
Neil Grogan <neil@grogan.ie>
2020-01-11 18:02:01 +0000

    Update add-torrent.sh

--- config/webhook/add-torrent.sh
+++ config/webhook/add-torrent.sh
@@ -5,7 +5,7 @@ exec &>/dev/stdout
 test -z $1 && echo "need magnet link! $0 <magnet link>" && exit 1
 test -z $2 && echo "need directory destination! $0 <magnet link> <destination>" && exit 1
 
-DOWNLOAD_DIR=/downloads/
+DOWNLOAD_DIR=/downloads/complete/
 DOWNLOAD_DIR=$DOWNLOAD_DIR$2
 
 TRANSMISSION=/usr/bin/transmission-remote
