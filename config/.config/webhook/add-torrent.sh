6bf7827317c29bf5bc015643c33bd2d6448e739c
Neil Grogan <neil@grogan.ie>
2020-01-11 22:29:38 +0000

    Make sure type is lower case

--- config/.config/webhook/add-torrent.sh
+++ config/.config/webhook/add-torrent.sh
@@ -5,8 +5,9 @@ exec &>/dev/stdout
 test -z $1 && echo "need magnet link! $0 <magnet link>" && exit 1
 test -z $2 && echo "need directory destination! $0 <magnet link> <destination>" && exit 1
 
-DOWNLOAD_DIR=/downloads/complete/
-DOWNLOAD_DIR=$DOWNLOAD_DIR$2
+PREFIX_DIR=/downloads/complete/
+TYPE_DIR=$(echo $2 | tr '[:upper:]' '[:lower:]')
+DOWNLOAD_DIR=$PREFIX_DIR/$TYPE_DIR
 
 TRANSMISSION=/usr/bin/transmission-remote
 
